dd917b6f74aa74092afafc3a970df3e45fb8a020
ashishprajapati <ashishprajapati012@gmail.com>
2025-05-11 15:29:07 +0530

    update work

---
+++ cypress/Integration/examples/Test8Framework.js
@@ -0,0 +1,12 @@
+describe('End to end ecommerce test', () => {
+    it("submit order",function(){
+
+        cy.visit("https://rahulshettyacademy.com/loginpagePractise/")
+        cy.get("#username").type("rahulshettyacademy")
+        cy.get("#password").type("learning")
+        cy.contains("Sign In").click()
+        cy.contains("Shop Name").should('be.visible')
+        cy.get('app-card').should('have.length',4)
+
+    })
+})
