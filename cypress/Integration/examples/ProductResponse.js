c299744ca0fe2e778f95a9a951e8c2fadbc92697
ashishprajapati <ashishprajapati012@gmail.com>
2025-10-10 11:52:24 +0530

    update file

--- cypress/Integration/examples/ProductResponse.js
+++ cypress/Integration/examples/ProductResponse.js
@@ -26,51 +26,51 @@ describe('ProductResponse', () => {
     //     cy.window().then(win => win.sessionStorage.clear());
     // });
 
-    for (let i = 1; i <= 1; i++) {
+    for (let i = 1; i <= 1000; i++) {
 
-        it('DemoX form and clicks hotspots', () => {
-            // Visit the URL
-            cy.visit('https://app.uzera.com/demoX/JS0A9XT00U/5ff617ab-0699-448f-aa58-9b245e26047b/share',{
-                onBeforeLoad(win) {
-                    win.localStorage.clear();
-                    win.sessionStorage.clear();
-                }
-            });
-            cy.wait(5000);
+        // it('DemoX form and clicks hotspots', () => {
+        //     // Visit the URL
+        //     cy.visit('https://app.uzera.com/demoX/JS0A9XT00U/5ff617ab-0699-448f-aa58-9b245e26047b/share',{
+        //         onBeforeLoad(win) {
+        //             win.localStorage.clear();
+        //             win.sessionStorage.clear();
+        //         }
+        //     });
+        //     cy.wait(5000);
 
-            // Generate random name & email
-            const randomName = `User${Math.floor(Math.random() * 10000)}`;
-            const randomEmail = `user${Math.floor(Math.random() * 10000)}@example.com`;
+        //     // Generate random name & email
+        //     const randomName = `User${Math.floor(Math.random() * 10000)}`;
+        //     const randomEmail = `user${Math.floor(Math.random() * 10000)}@example.com`;
 
-            // Fill name field
-            cy.get('#\\:r3\\:').type(randomName);
-            cy.wait(2000);
+        //     // Fill name field
+        //     cy.get('#\\:r3\\:').type(randomName);
+        //     cy.wait(2000);
 
-            // Fill email field
-            cy.get('#\\:r5\\:').type(randomEmail);
-            cy.wait(2000);
+        //     // Fill email field
+        //     cy.get('#\\:r5\\:').type(randomEmail);
+        //     cy.wait(2000);
 
-            // Click continue button
-            cy.get(
-                '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > ' +
-                'div > div.MuiBox-root.css-15qx0d1 > div > div > div.MuiStack-root.css-gfhfdc > button'
-            ).click();
-            cy.wait(4000);
+        //     // Click continue button
+        //     cy.get(
+        //         '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > ' +
+        //         'div > div.MuiBox-root.css-15qx0d1 > div > div > div.MuiStack-root.css-gfhfdc > button'
+        //     ).click();
+        //     cy.wait(4000);
 
-            // Hotspot clicks
-            const hotspotSelectors = [
-                '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div.MuiBox-root.css-3al35z > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-zbb78r > div > svg',
-                '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-1cieebd > div > svg',
-                '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-1hs1t6f > div > svg',
-                '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-zaksfz > div > svg'
-            ];
+        //     // Hotspot clicks
+        //     const hotspotSelectors = [
+        //         '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div.MuiBox-root.css-3al35z > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-zbb78r > div > svg',
+        //         '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-1cieebd > div > svg',
+        //         '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-1hs1t6f > div > svg',
+        //         '#ul-demox-embed > div > div > div > div > div > div.MuiBox-root.css-1js65af > div > div.DemoxScript_gs-tooltip-Wrapper__lxSIu.DemoxScript_animation-view__sKxhM.MuiBox-root.css-zaksfz > div > svg'
+        //     ];
 
-            hotspotSelectors.forEach((selector) => {
-                cy.get(selector).click({ force: true });
-                cy.wait(4000);
-            });
+        //     hotspotSelectors.forEach((selector) => {
+        //         cy.get(selector).click({ force: true });
+        //         cy.wait(4000);
+        //     });
 
-        });
+        // });
 
         it('Anonymous User who complete everything', () => {
 
