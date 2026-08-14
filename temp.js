74c087fd41eac139011cdf8cedce99c510e805d6
ashish.prajapati <ashish.prajapati@gainserv.com>
2026-08-14 15:39:57 +0530

    latest violation

--- temp.js
+++ temp.js
@@ -2,3 +2,15 @@ for (let i = 1; i <= 20; i++) {
   console.log(i);
 }
  kjnjkaddkhajkdadhnkjasdnk
+
+ if (age == "18") { console.log("Adult"); }
+ 
+var name = "John";
+ 
+let city = "Ahmedabad"; console.log(city); // never reassigned
+ 
+function greet() { let unusedVar = 10; console.log("Hello"); }
+ 
+if (isValid) console.log("Valid");
+ 
+console.log("Debugging info");
